9543deedf6a460d31a50dfe4e3c68de40a275a6a
raskopova <raskopovaa@gmail.com>
2022-07-10 19:38:09 +0700

    dice roll

---
+++ app/src/main/java/com/example/myfirstandroidapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myfirstandroidapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myfirstandroidapplication.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drowableRes = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drowableRes)
+    }
+}
